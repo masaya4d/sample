980a387c3d2bb52df8d66a8d3ee18bf9895ec365
Akihiro Masata <masaya@4d-sensor.com>
2017-01-30 13:04:14 +0900

    最初のコミット

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "glfw3.lib")
+
+int main(int argc, char *argv[])
+{
+	if (glfwInit() == GL_FALSE)
+	{
+		MessageBox(nullptr, TEXT("GLFW ‚ª‰Šú‰»‚Å‚«‚Ü‚¹‚ñ"), TEXT("4D Sensor"), MB_OK);
+		return EXIT_FAILURE;
+	}
+	atexit(glfwTerminate);
+
+	return EXIT_SUCCESS;
+}
